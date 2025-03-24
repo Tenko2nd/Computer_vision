601275a276b8baf8e7f2d23e525acbc3c72939cf
Mathis Cassert <mathis.cassert@reseau.eseo.fr>
2025-03-24 17:26:37 +0100

    fin scéance 1

--- homographie.py
+++ homographie.py
@@ -9,6 +9,31 @@ def homographies(img):
     im_dst = cv.warpPerspective(img, h, (tab_w*factor,tab_h*factor))
     return im_dst
 
+def isolation(img):
+    low_mousse = np.array([100,85,85])
+    high_mousse = np.array([155,255,175])
+    hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
+    mask = cv.inRange(hsv, low_mousse, high_mousse)
+    mask = cv.dilate(mask, kernel=np.ones((2, 2), np.uint8), iterations=4)
+    res = cv.bitwise_and(img, img, mask=mask)
+    res = apply_moments(mask, res)
+    return res
+
+def apply_moments(mask, img):
+    moments = cv.moments(mask)
+    if moments['m00'] == 0:
+        return img
+    """ Centroid """
+    centroid = (int(moments['m10']/moments['m00']),int(moments['m01']/moments['m00']))
+    img = cv.circle(img, centroid, radius=5, color=(255, 0, 0), thickness=-1)
+
+    contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    cnt = max(contours, key=cv.contourArea)
+    ellipse = cv.fitEllipse(cnt)
+    cv.ellipse(img,ellipse,(0,255,0),2)
+
+    return img
+
 cap = cv.VideoCapture("Ressources/Video/Mousse.mp4")
 while cap.isOpened():
     # Capture frame-by-frame
@@ -16,6 +41,7 @@ while cap.isOpened():
     if ret == True:
         # Display the resulting frame
         frame = homographies(frame)
+        frame = isolation(frame)
         cv.imshow('Frame', frame)
         # Press Q on keyboard to exit
         if cv.waitKey(200) & 0xFF == ord('q'):
